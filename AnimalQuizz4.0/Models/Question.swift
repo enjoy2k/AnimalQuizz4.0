339366e635975a2333b80470578de315e4f02181
enjoy2k <61755950+enjoy2k@users.noreply.github.com>
2023-07-29 18:01:03 +0300

    Написал модель

--- AnimalQuizz4.0/Models/Question.swift
+++ AnimalQuizz4.0/Models/Question.swift
@@ -5,5 +5,74 @@
 //  Created by Даниил Козлов on 29.07.2023.
 //
 
-import Foundation
+struct Question {
+    let title: String
+    let type: ResponseType
+    let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "какую пищу вы предпочитатете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Кушать", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .rabbit),
+                    Answer(title: "Не заамечаю", animal: .turtle),
+                    Answer(title: "Обожаю", animal: .dog)
+                ]
+            )
+        ]
+    }
+}
 
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вы замечательный дружище! Очень верный и дружелюбный!"
+        case .cat:
+            return "Вы себе на уме. Любите спать, не любите играть, любите чистоту."
+        case .rabbit:
+            return "Вы бодры и активны. Любите природу и чпокаться"
+        case .turtle:
+            return "Вы мудры и не торопливы. Уг-Вэй гордится Вами"
+        }
+    }
+}
