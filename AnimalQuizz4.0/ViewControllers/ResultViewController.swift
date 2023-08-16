afd0a569952d613c58a9ccf74b3885b40f41c1c8
enjoy2k <61755950+enjoy2k@users.noreply.github.com>
2023-08-16 17:06:16 +0300

    корректировки

--- AnimalQuizz4.0/ViewControllers/ResultViewController.swift
+++ AnimalQuizz4.0/ViewControllers/ResultViewController.swift
@@ -9,11 +9,6 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    //    Передать массив с ответами
-    //    Определить наиболле часто встречающееся животное
-    //    Отобразить результаты по этому зверю
-    
-    
     @IBOutlet var animalTitleLabel: UILabel!
     @IBOutlet var animalDescriptionLabel: UILabel!
     
@@ -48,39 +43,4 @@ class ResultViewController: UIViewController {
         }
         return mostFrequentAnimal
     }
-    
-    
-    
-    
-    
 }
-    /*
-     Есть массив с ответами Answer.title + .animal
-         
-     Создаю словарь, чтобы вынуть из ответов Животное, и присвоить ему цифу
-
-     После этого мне нужно отсортировать словарь по количеству упоминаний животного
-
-     А после передать в лэйбл animal.definition и animal.?
-     
-     
-     
-     
-     
-     
-     let dictionary = ["one": 1, "three": 3, "five": 5, "two": 2, "four": 4]
-
-     let sortedDictionary = dictionary.sorted { (firstPair, secondPair) -> Bool in
-         return firstPair.key < secondPair.key
-     }
-
-     for (key, value) in sortedDictionary {
-         print("\(key): \(value)")
-     }
-     
-     
-     при помощи метода updateValue() Обновляю значение в определённом ключе-животном
-     Далее ты сортируешь словарь по его значению, и потом берешь первый ключ, это и будет результат.
-     
-     Массив нужно перебрать и в качестве ключа определить элемент массива а в качестве значения их количество, одним словом делаешь проверку если такой в словаре уже есть, то просто обновляешь значение в словаре при помощи метода updateValue
-     */
