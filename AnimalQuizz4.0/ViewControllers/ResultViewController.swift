0376d15bf585bfd3a0a716bd67588d95cf096bde
enjoy2k <61755950+enjoy2k@users.noreply.github.com>
2023-08-21 19:03:15 +0300

    Закончил

--- AnimalQuizz4.0/ViewControllers/ResultViewController.swift
+++ AnimalQuizz4.0/ViewControllers/ResultViewController.swift
@@ -16,9 +16,8 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.setHidesBackButton(true, animated: true)
-        
-        updateUI(wirh: )
+        navigationItem.hidesBackButton = true
+        updateResult()
     }
     
     private func updateResult() {
@@ -34,12 +33,24 @@ class ResultViewController: UIViewController {
             }
         }
         
+            /*               Можно и так. Только прочесть будет сложнее
+             for animal in animals {
+             frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+             }
+             */
+        
+            /*        Решение в одну строку. Это читается ещё сложнее, и зависит от того кто будет работать с этим кодом.
+             let mostFrequencyAnimal = Dictionary(grouping: answers) { $0.animal } .sorted { $0.value.count > $1.value.count } .firs?.key
+             */
+        
         let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
         guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        
+        updateUI(wirh: mostFrequencyAnimal)
     }
     
     private func updateUI(wirh animal: Animal) {
-        animalTitleLabel.text = "Вы - \(animal.rawValue)"
+        animalTitleLabel.text = "Вы - \(animal.rawValue)!"
         animalDescriptionLabel.text = animal.definition
     }
 }
